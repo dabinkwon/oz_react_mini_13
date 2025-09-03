9538e99db1a73a39cf8c1ad816b86bffac9cf9cc
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-09-03 23:57:05 +0900

    refactor: movieDetailData를 상태로 관리하고 주석 추가

--- movie-project/pages/Detail.jsx
+++ movie-project/pages/Detail.jsx
@@ -1,33 +1,40 @@
 import { imgUrl } from "../url/imgUrl";
 import movieDetailData from "../data/movieDetailData.json";
+import { useState } from "react";
 
 export default function Detail() {
-  const genre = movieDetailData.genres.map((el) => el.name).join(", ");
+  const [movieDetail, setMovieDetail] = useState({
+    title: movieDetailData.title,
+    genres: movieDetailData.genres.map((el) => el.name).join(", "),
+    backgroundImage: `${imgUrl}${movieDetailData.backdrop_path}`,
+    overview: movieDetailData.overview,
+    voteAvg: movieDetailData.vote_average,
+  });
 
   return (
+    // Detail page 배경으로 영화 img넣어줌
     <div
       className="relative min-h-screen bg-cover bg-center flex flex-col items-center p-5"
       style={{
         backgroundImage: `
       linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)),
-      url(${imgUrl}${movieDetailData.backdrop_path})
-    `,
+      url(${imgUrl}${movieDetail.backgroundImage})`,
       }}
     >
       <h1 className="text-center mb-4 text-3xl font-bold text-white">
-        {movieDetailData.title}
+        {movieDetail.title}
       </h1>
-
+      {/* detail컴포넌트로 파일 분리시켜보기!!! */}
       <div className="flex flex-col items-center justify-center gap-6 p-7 bg-gray-500/50 rounded-xl">
         <img
           className="rounded-lg"
-          src={`${imgUrl}${movieDetailData.backdrop_path}`}
-          alt={movieDetailData.title}
+          src={`${imgUrl}${movieDetail.backgroundImage}`}
+          alt={movieDetail.title}
         />
-        <p>{movieDetailData.vote_average}</p>
-        <p>{genre}</p>
+        <p>{movieDetail.voteAvg}</p>
+        <p>{movieDetail.genres}</p>
         <p className="text-white max-w-[70vw] text-center">
-          {movieDetailData.overview}
+          {movieDetail.overview}
         </p>
       </div>
     </div>
