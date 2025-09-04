37564d68c01ddd1f3d3c8b032468969348b673e7
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-09-04 17:06:21 +0900

    feat: Context API로 상태를 전역에서 관리하도록 구현

---
+++ movie-project/context/MovieContext.jsx
@@ -0,0 +1,26 @@
+import { createContext } from "react";
+import useFetch from "../hooks/useFetch"
+
+export const MovieContext = createContext();
+
+export function MovieProvider({children}){
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODI2ZjVkOTZmMDY3ZjE2MmFkYWViZGQzZGE3ZGJmNyIsIm5iZiI6MTc1Njk1ODU4My4wODQsInN1YiI6IjY4YjkwZjc3MTU4ZTU2NjkyYTliYjI1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wYBV7FzivAFbGNs2coFwwVz0M0fJhx3J4HatiSC345I",
+      },
+    };
+
+    const [data, loading, error] = useFetch(
+      "https://api.themoviedb.org/3/movie/popular?language=ko&page=1",
+      options
+    );
+    
+  return (
+    <MovieContext value={{data,loading,error}}>
+        {children}
+    </MovieContext>
+  )
+}
--- movie-project/pages/Main.jsx
+++ movie-project/pages/Main.jsx
@@ -1,24 +1,12 @@
+import { useContext } from "react";
 import MovieCard from "../component/MovieCard";
-import useFetch from "../hooks/useFetch";
+import { MovieContext } from "../context/MovieContext";
 
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODI2ZjVkOTZmMDY3ZjE2MmFkYWViZGQzZGE3ZGJmNyIsIm5iZiI6MTc1Njk1ODU4My4wODQsInN1YiI6IjY4YjkwZjc3MTU4ZTU2NjkyYTliYjI1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wYBV7FzivAFbGNs2coFwwVz0M0fJhx3J4HatiSC345I",
-  },
-};
 
 export default function Main() {
-  // const [movie,setMovie] = useState(data)
-  const [data, loading, error] = useFetch(
-    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-    options
-  );
+  const {data,loading,error} = useContext(MovieContext)
 
   const movieForAll = data?.results.filter(el=>el.adult === false)
-  console.log(movieForAll)
 
   if(error)return <div className="text-center font-bold text-3xl pt-10">Error : 페이지를 찾 을 수 없습니다.</div>;
 
